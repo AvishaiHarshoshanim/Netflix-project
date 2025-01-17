503443b23ff9c0bca77c42c9938250495fcdcbab
avishai-git <hah2hah@gmail.com>
2025-01-17 15:10:17 +0200

    admin page activity connects to server

--- android_app/app/src/main/java/com/example/android_app/MainActivity.java
+++ android_app/app/src/main/java/com/example/android_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // David and lior! replace "AdminPagePackage.AdminPage" to your page
+        Intent intent = new Intent(MainActivity.this, AdminPagePackage.AdminPage.class);
+        startActivity(intent);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
